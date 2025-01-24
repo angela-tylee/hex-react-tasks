3c29d4b3709f7cd0a2212cd3b26bbaec176ce97c
Angela Li <angela.tylee25@gmail.com>
2025-01-25 02:26:28 +0800

    Updated week2 `vite.config.js`

--- week2/vite.config.js
+++ week2/vite.config.js
@@ -1,7 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { fileURLToPath } from 'node:url';
+import path from 'node:path';
+import { glob } from 'glob';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/hex-react-tasks/week2/dist/',
   plugins: [react()],
+  server: {
+    open: '/',
+  },
+  build: {
+    rollupOptions: {
+      input: Object.fromEntries(
+        glob
+          .sync('*.html')
+          .map((file) => [
+            path.basename(file, path.extname(file)),
+            fileURLToPath(new URL(file, import.meta.url)),
+          ])
+      ),
+    },
+    outDir: 'dist',
+  },
 })
