e9db88a49d2f726e007fe0792108303683ad2ef9
Angela Li <angela.tylee25@gmail.com>
2025-01-25 21:11:49 +0800

    Added week4 files

---
+++ week4/src/components/Pagination.jsx
@@ -0,0 +1,43 @@
+const Pagination = ({ pagination, paginationTotal, changePage }) => {
+  console.log(pagination);
+  return (
+    <nav aria-label="Page navigation example" className="mt-5">
+      <ul className="pagination justify-content-center">
+        <li className={`page-item ${pagination.has_pre ? '' : 'disabled'}`}>
+          <a className="page-link" href="#">
+            Previous
+          </a>
+        </li>
+        {Array.from({ length: paginationTotal }).map((_, index) => (
+          <li
+            key={index}
+            className={`page-item ${
+              pagination.current_page === index + 1 ? 'active' : ''
+            }`}
+            aria-current={
+              pagination.current_page === index + 1 ? 'page' : undefined
+            }
+          >
+            <a
+              className="page-link"
+              href="#"
+              onClick={(e) => {
+                e.preventDefault();
+                changePage(index + 1);
+              }}
+            >
+              {index + 1}
+            </a>
+          </li>
+        ))}
+        <li className={`page-item ${pagination.has_next ? '' : 'disabled'}`}>
+          <a className="page-link" href="#">
+            Next
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Pagination;
--- week4/src/components/ProductModal.jsx
+++ week4/src/components/ProductModal.jsx
@@ -1,16 +1,12 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect } from 'react';
 
-const API_BASE = 'https://ec-course-api.hexschool.io/v2';
-const API_PATH = 'angela-carpento';
+const ProductModal = ({ type, tempProduct, closeProductModal, getProducts }) => {
 
-function App() {
-  const [formData, setFormData] = useState({
-    username: '',
-    password: '',
-  });
-  const [isAuth, setIsAuth] = useState(false);
-  const [products, setProducts] = useState([]);
-  const [tempProduct, setTempProduct] = useState({});
+  // const API_BASE = 'https://ec-course-api.hexschool.io/v2';
+  // const API_PATH = 'angela-carpento';
+
+  const {VITE_API_BASE, VITE_API_PATH} = import.meta.env;
+
   const [tempData, setTempData] = useState({
     title: '',
     category: '',
@@ -28,25 +24,6 @@ function App() {
     imagesUrl: [],
     tag: '',
   });
-  const [type, setType] = useState('');
-  const productModalRef = useRef(null);
-
-  useEffect(() => {
-    const token = document.cookie
-      .split('; ')
-      .find((row) => row.startsWith('hexToken='))
-      ?.split('=')[1];
-
-    axios.defaults.headers.common['Authorization'] = token;
-
-    checkAdmin();
-
-    productModalRef.current = new window.bootstrap.Modal('#productModal', {
-      keyboard: false,
-    });
-
-    getProducts();
-  }, []);
 
   useEffect(() => {
     if (type === 'create') {
@@ -72,16 +49,6 @@ function App() {
     }
   }, [type, tempProduct]);
 
-  function openProductModal(type, product) {
-    setTempProduct(product);
-    setType(type);
-    productModalRef.current.show();
-  }
-
-  function closeProductModal() {
-    productModalRef.current.hide();
-  }
-
   const handleProductInputChange = (e) => {
     const { name, value, checked } = e.target;
 
@@ -103,8 +70,8 @@ function App() {
         ...tempData,
         [name]: Number(checked),
       });
-      // } else if (name === 'imagesUrl') {
-      //   addImages(e);
+    // } else if (name === 'imagesUrl') {
+    //   addImages(e);
     } else {
       setTempData({
         ...tempData,
@@ -113,7 +80,7 @@ function App() {
     }
   };
 
-  const [imageUrl, setImageUrl] = useState('');
+  const [ imageUrl, setImageUrl] = useState("");
 
   const addImages = (e) => {
     e.preventDefault();
@@ -137,15 +104,15 @@ function App() {
       images.pop();
       return { ...prevData, imagesUrl: images };
     });
-  };
+  }
 
   const handleProductSubmit = async () => {
     try {
-      let api = `${API_BASE}/api/${API_PATH}/admin/product`;
+      let api = `${VITE_API_BASE}/api/${VITE_API_PATH}/admin/product`;
       let method = 'post';
       if (type === 'edit') {
         method = 'put';
-        api = `${API_BASE}/api/${API_PATH}/admin/product/${tempProduct.id}`;
+        api = `${VITE_API_BASE}/api/${VITE_API_PATH}/admin/product/${tempProduct.id}`;
       }
       const res = await axios[method](api, {
         data: tempData,
@@ -159,170 +126,8 @@ function App() {
     }
   };
 
-  const deleteProduct = async (id) => {
-    try {
-      const res = await axios.delete(
-        `${API_BASE}/api/${API_PATH}/admin/product/${id}`
-      );
-      console.log(res);
-      alert(res.data.message);
-      getProducts();
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const checkAdmin = async () => {
-    try {
-      await axios.post(`${API_BASE}/api/user/check`);
-      setIsAuth(true);
-    } catch (err) {
-      console.log(err.response.data.message);
-    }
-  };
-
-  const handleInputChange = (e) => {
-    const { id, value } = e.target;
-    setFormData((prevData) => ({
-      ...prevData,
-      [id]: value,
-    }));
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await axios.post(`${API_BASE}/admin/signin`, formData);
-      const { token, expired } = response.data;
-      document.cookie = `hexToken=${token};expires=${new Date(expired)};`;
-      axios.defaults.headers.common.Authorization = token;
-      setIsAuth(true);
-    } catch (error) {
-      alert('登入失敗: ' + error.response.data.message);
-    }
-  };
-
-  const getProducts = async () => {
-    try {
-      const res = await axios.get(`${API_BASE}/api/${API_PATH}/admin/products`);
-      console.log(products);
-      setProducts(res.data.products);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
   return (
-    <>
-      {isAuth ? (
-        <div>
-          <div className="container">
-            <div className="text-end mt-4">
-              <button
-                className="btn btn-primary"
-                onClick={() => openProductModal('create', {})}
-              >
-                建立新的產品
-              </button>
-            </div>
-            <table className="table mt-4">
-              <thead>
-                <tr>
-                  <th width="120">分類</th>
-                  <th>產品名稱</th>
-                  <th width="120">原價</th>
-                  <th width="120">售價</th>
-                  <th width="100">是否啟用</th>
-                  <th width="120">編輯</th>
-                </tr>
-              </thead>
-              <tbody>
-                {products &&
-                  products.map((product) => (
-                    <tr key={product.id}>
-                      <td>{product.category}</td>
-                      <td>{product.title}</td>
-                      <td className="text-end">{product.origin_price}</td>
-                      <td className="text-end">{product.price}</td>
-                      <td>
-                        {product.is_enabled ? (
-                          <span className="text-success">啟用</span>
-                        ) : (
-                          <span>未啟用</span>
-                        )}
-                      </td>
-                      <td>
-                        <div className="btn-group">
-                          <button
-                            type="button"
-                            className="btn btn-outline-primary btn-sm"
-                            onClick={() => {
-                              openProductModal('edit', product);
-                            }}
-                          >
-                            編輯
-                          </button>
-                          <button
-                            type="button"
-                            className="btn btn-outline-danger btn-sm"
-                            onClick={() => {
-                              deleteProduct(product.id);
-                            }}
-                          >
-                            刪除
-                          </button>
-                        </div>
-                      </td>
-                    </tr>
-                  ))}
-              </tbody>
-            </table>
-          </div>
-        </div>
-      ) : (
-        <div className="container login">
-          <div className="row justify-content-center">
-            <h1 className="h3 mb-3 font-weight-normal">請先登入</h1>
-            <div className="col-8">
-              <form id="form" className="form-signin" onSubmit={handleSubmit}>
-                <div className="form-floating mb-3">
-                  <input
-                    type="email"
-                    className="form-control"
-                    id="username"
-                    placeholder="name@example.com"
-                    value={formData.username}
-                    onChange={handleInputChange}
-                    required
-                    autoFocus
-                  />
-                  <label htmlFor="username">Email address</label>
-                </div>
-                <div className="form-floating">
-                  <input
-                    type="password"
-                    className="form-control"
-                    id="password"
-                    placeholder="Password"
-                    value={formData.password}
-                    onChange={handleInputChange}
-                    required
-                  />
-                  <label htmlFor="password">Password</label>
-                </div>
-                <button
-                  className="btn btn-lg btn-primary w-100 mt-3"
-                  type="submit"
-                >
-                  登入
-                </button>
-              </form>
-            </div>
-          </div>
-          <p className="mt-5 mb-3 text-muted">&copy; 2024~∞ - 六角學院</p>
-        </div>
-      )}
-      <div
+    <div
         id="productModal"
         className="modal fade"
         tabIndex="-1"
@@ -333,7 +138,7 @@ function App() {
           <div className="modal-content border-0">
             <div className="modal-header bg-dark text-white">
               <h5 id="productModalLabel" className="modal-title">
-                {type === 'edit' ? (
+              {type === 'edit' ? (
                   <span>編輯產品</span>
                 ) : (
                   <span>新增產品</span>
@@ -361,32 +166,21 @@ function App() {
                         id="imageUrl"
                         name="imageUrl"
                         value={imageUrl}
-                        onChange={(e) => {
-                          setImageUrl(e.target.value);
-                        }}
+                        onChange={(e) => {setImageUrl(e.target.value)}}
                       />
                     </div>
-                    {tempData.imagesUrl &&
-                      tempData.imagesUrl.map((image, index) => (
-                        <img
-                          key={index}
-                          className="img-fluid"
-                          src={image}
-                          alt=""
-                        />
-                      ))}
+                    {tempData.imagesUrl && tempData.imagesUrl.map((image, index) => (
+                      <img key={index} className="img-fluid" src={image} alt="" />
+                    ))}
                     <button className="btn btn-outline-primary btn-sm d-block w-100">
                       新增圖片
                     </button>
                   </form>
-                  <div>
-                    <button
-                      className="btn btn-outline-danger btn-sm d-block w-100"
-                      onClick={removeImage}
-                    >
-                      刪除圖片
-                    </button>
-                  </div>
+                    <div>
+                      <button className="btn btn-outline-danger btn-sm d-block w-100" onClick={removeImage}>
+                        刪除圖片
+                      </button>
+                    </div>
                 </div>
                 <div className="col-sm-8">
                   <div className="mb-3">
@@ -547,21 +341,14 @@ function App() {
               >
                 取消
               </button>
-              <button
-                type="button"
-                className="btn btn-primary"
-                onClick={handleProductSubmit}
-              >
+              <button type="button" className="btn btn-primary" onClick={handleProductSubmit}>
                 確認
               </button>
             </div>
           </div>
         </div>
       </div>
-    </>
-  );
+  )
 }
 
-// ReactDOM.render(<App />, document.getElementById("root"));
-
-export default App;
+export default ProductModal;
